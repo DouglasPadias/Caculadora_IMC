78f78219ee70c03beef24f64331e2bb46b3e472b
Douglas Padias <149883810+DouglasPadias@users.noreply.github.com>
2025-04-20 12:48:04 -0300

    Primeiro commit do projeto

---
+++ imc_calc/src/App.jsx
@@ -0,0 +1,14 @@
+// importando componentes:
+import { ImcCalc } from "./components/ImcCalc";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <ImcCalc />
+    </div>
+  );
+}
+
+export default App;
---
+++ imc_calc/src/components/ImcCalc.jsx
@@ -0,0 +1,9 @@
+// Estrutura HTML do site
+
+export const ImcCalc = () => {
+  return (
+    <div className="container">
+      <h2>Calculadora IMC</h2>
+    </div>
+  );
+};
